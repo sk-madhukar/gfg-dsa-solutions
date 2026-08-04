a7c37b5e6e8c669a0726a437158332c3639c7879
Shubham Kr. Madhukar <91412635+sk-madhukar@users.noreply.github.com>
2026-08-04 21:04:16 +0530

    Update GFG solution: Count Only Repeated

--- Medium/Count Only Repeated/count-only-repeated2047.cs
+++ Medium/Count Only Repeated/count-only-repeated2047.cs
@@ -7,6 +7,7 @@ class Solution {
         // code here
         
         int n = arr.Length;
+        /*
         // int[] freq = new int[n+1];
         // for(int i=0; i<n; i++){
         //     freq[arr[i]]++;
@@ -18,21 +19,82 @@ class Solution {
         // }
         // return new List<int>{-1, -1};
         
+        // int repeated = -1;
+        // int count = -1;
+        // int i = 1;
+        // for(; i<n; i++){
+        //     if(arr[i] == arr[i-1]){
+        //         repeated = arr[i];
+        //         count = 2;
+        //         break;
+        //     }
+        // }
+        // int j = i+1;
+        // while(j<n && arr[j] == repeated){
+        //     count++;
+        //     j++;
+        // }
+        // return new List<int>{repeated, count};
+        */
+        
         int repeated = -1;
-        int count = -1;
-        int i = 1;
-        for(; i<n; i++){
-            if(arr[i] == arr[i-1]){
-                repeated = arr[i];
-                count = 2;
-                break;
+        int low = 0;
+        int high = n-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(arr[mid] == arr[0]+mid){
+                low = mid+1;
+            }
+            else{
+                high = mid;
             }
         }
-        int j = i+1;
-        while(j<n && arr[j] == repeated){
-            count++;
-            j++;
+        
+        if(low == 0 || arr[low] != arr[low-1]){
+            return new List<int>{-1, -1};
         }
+        
+        repeated = arr[low];
+        int count = (LastOcc(arr, repeated)-FirstOcc(arr, repeated)) + 1;
+        
         return new List<int>{repeated, count};
+    }
+    private int FirstOcc(int[] arr, int x){
+        int ans = -1;
+        int low = 0;
+        int high = arr.Length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==x){
+                ans = mid;
+                high = mid-1;
+            }
+            else if(arr[mid] < x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+    private int LastOcc(int[] arr, int x){
+        int ans = -1;
+        int low = 0;
+        int high = arr.Length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==x){
+                ans = mid;
+                low = mid+1;
+            }
+            else if(arr[mid] < x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
     }
 }
